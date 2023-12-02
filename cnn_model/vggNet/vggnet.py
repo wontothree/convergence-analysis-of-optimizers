d054bca8306013cc260839857e80715fb81685dc
williamkevin <prepared7913@naver.com>
2023-12-02 14:30:15 +0900

    -

---
+++ cnn_model/vggNet/vggnet.py
@@ -0,0 +1,39 @@
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Model
+import visualkeras
+
+_input = Input((224,224,1)) 
+
+conv1  = Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu")(_input)
+conv2  = Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu")(conv1)
+pool1  = MaxPooling2D((2, 2))(conv2)
+
+conv3  = Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu")(pool1)
+conv4  = Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu")(conv3)
+pool2  = MaxPooling2D((2, 2))(conv4)
+
+conv5  = Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu")(pool2)
+conv6  = Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu")(conv5)
+conv7  = Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu")(conv6)
+pool3  = MaxPooling2D((2, 2))(conv7)
+
+conv8  = Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu")(pool3)
+conv9  = Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu")(conv8)
+conv10 = Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu")(conv9)
+pool4  = MaxPooling2D((2, 2))(conv10)
+
+conv11 = Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu")(pool4)
+conv12 = Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu")(conv11)
+conv13 = Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu")(conv12)
+pool5  = MaxPooling2D((2, 2))(conv13)
+
+flat   = Flatten()(pool5)
+dense1 = Dense(4096, activation="relu")(flat)
+dense2 = Dense(4096, activation="relu")(dense1)
+output = Dense(1000, activation="softmax")(dense2)
+
+vgg16_model  = Model(inputs=_input, outputs=output)
+
+vgg16_model.summary()
+visualkeras.layered_view(vgg16_model)
